3cae2fba15ccb8066b6bb4928ec49f7bc9e822df
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-17 14:35:18 +0200

    add button to restart function

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-const listEmail = document.querySelectorAll('.emailList > li')
-//console.log(listEmail);
+const emaiilListEl = document.querySelectorAll('.emailList > li')
+//console.log(emaiilListEl);
+const resetEl = document.querySelector('.reset')
+//console.log(resetEl);
+
 
 
 
 //ciclo per iterare all interno della node LI 
-listEmail.forEach((element, index)=> {
-    //chiamata axios per generare randomicamente un email 
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response =>{
-            //aggiungiamo ad LI un EMAIL
-            element.textContent = response.data.response;
-            //console.log(element);
-        })
-});
-   
+function generateRandomEmail() {
+    emaiilListEl.forEach((element, index) => {
+        //chiamata axios per generare randomicamente un email 
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => {
+                //aggiungiamo ad LI un EMAIL
+                element.textContent = response.data.response;
+                //console.log(element);
+            })
+    })
+}
+;
+//event listener per restartare la funzione
+resetEl.addEventListener('click', generateRandomEmail);
+//richiamo della funzione per generare 10 email al caricamento della pagina
+generateRandomEmail()
+
+
+
 
