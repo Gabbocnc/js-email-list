edd645326dc5139689f592f790b4f3eed0162e40
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-17 14:13:22 +0200

    add forEach loop

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const listEl = document.querySelectorAll('.emailList > li')
-console.log(listEl);
+const listEmail = document.querySelectorAll('.emailList > li')
+//console.log(listEmail);
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 
-/* .then(response => {
-    const data = (response.data.response)
-})
- */
+
+//ciclo per iterare all interno della node LI 
+listEmail.forEach((element, index)=> {
+    //chiamata axios per generare randomicamente un email 
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response =>{
+            //aggiungiamo ad LI un EMAIL
+            element.textContent = response.data.response;
+            //console.log(element);
+        })
+});
+   
 
